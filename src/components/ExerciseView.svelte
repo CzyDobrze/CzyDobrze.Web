1d42f4a11c7fa05d9b0a807dc3d139ece8e38b62
Piotr Trybisz <ptrybisz@gmail.com>
2021-10-20 23:18:31 +0200

    Added Comments subpage

--- src/components/ExerciseView.svelte
+++ src/components/ExerciseView.svelte
@@ -63,6 +63,28 @@
                 ]
             }
     });
+    function like(){
+        // TODO: send like
+        currentExercise.answers[0].votes.push({
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+            "created": "2021-10-19T20:30:20.913Z",
+            "updated": "2021-10-19T20:30:20.913Z",
+            "voter": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "value": 1
+        })
+        currentExercise.answers[0].votes=currentExercise.answers[0].votes
+    }
+    function dislike(){
+        // TODO: send dislike
+        currentExercise.answers[0].votes.push({
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+            "created": "2021-10-19T20:30:20.913Z",
+            "updated": "2021-10-19T20:30:20.913Z",
+            "voter": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "value": -1
+        })
+        currentExercise.answers[0].votes=currentExercise.answers[0].votes
+    }
 </script>
 <div class="container max-w-5xl mx-auto px-4">
     <div class="w-4/5 mx-auto">
@@ -91,13 +113,13 @@
                             </svg>
                         </a>
                             &nbsp;
-                            <a href="#like">
+                            <a on:click={like} class="cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
                                 </svg>
                             </a>
-                            <b>{item.votes.length}</b>
-                            <a href="#dislike">
+                            <b>{item.votes.reduce((a, b) => +a + +b.value, 0)}</b>
+                            <a on:click={dislike} class="cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
                                 </svg>
